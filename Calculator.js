2727789f1f19865cfbbcf8c4af8737a4a0d940c1
Arthur Victory <arthurvictory@outlook.com>
2025-03-03 21:20:59 -0600

    first commit

---
+++ Calculator.js
@@ -0,0 +1,15 @@
+// Task 2
+
+//declare Variables for two numbers
+let num1 = 5;
+let num2 = 8;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
---
+++ studentManagement.js
@@ -0,0 +1,13 @@
+// Task 1
+
+// declare Variables
+let studentName = "Arthur";
+let studentAge = 44;
+let studentGrade = 100;
+let isPresent = true;
+
+// Console log info
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Grade:", studentGrade);
+console.log("Attendance:", isPresent);
