9ad6686cb22bfbed706b8cc76d83bda34a95d770
Arthur Victory <arthurvictory@outlook.com>
2025-03-05 14:57:17 -0600

    updated Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,10 @@
-// Task 2
+// Task #1 and #2
 
-//declare Variables for two numbers
+// declare Variables for two numbers
 let num1 = 5;
 let num2 = 8;
 
+// Task #3
 let sum = num1 + num2;
 let difference = num1 - num2;
 let product = num1 * num2;
@@ -13,3 +14,26 @@ console.log("Sum:", sum);
 console.log("Difference:", difference);
 console.log("Product:", product);
 console.log("Quotient:", quotient);
+
+// Task #4
+num1 = 7;
+num2 = 13;
+
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+// Task #5
+let isEqualTo = num1 == num2;
+let isGreaterThan = num1 > num2;
+let isNotEqual = num1 != num2;
+
+console.log("Is num1 equal to num2?", isEqualTo);
+console.log("Is num1 greater than num2?", isGreaterThan);
+console.log("Is num1 not equal to num2?", isNotEqual);
+
+// Task #6
+let isPositive = true || false;
+let isEven = true && false;
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
